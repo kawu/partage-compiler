0489e70af6b483f2ccdd56246a462a833bb0e64b
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2020-05-06 12:40:33 +0200

    Further cleanup (#5)

--- src/ParComp/Tests/CFG.hs
+++ src/ParComp/Tests/CFG.hs
@@ -2,278 +2,438 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE LambdaCase #-}
 
+{-# LANGUAGE ScopedTypeVariables #-}
+
 
 -- | Context-free grammar parsing
 
 
 module ParComp.Tests.CFG
   ( testCFG
+  , suffix
+  , testX
+  , testMatch
   ) where
 
 
-import           Prelude hiding (splitAt)
+import           Prelude hiding
+  (splitAt, span, map, or, and, any, const, head)
+import qualified Prelude as P
 
--- import           Control.Monad (forM_)
--- 
+import           Control.Monad (forM_)
+import qualified Control.Monad as P
+
 import qualified Data.Text as T
 import qualified Data.Map.Strict as M
 import qualified Data.Set as S
 import           Data.Maybe (fromJust)
 
-import qualified ParComp.Item as I
-import qualified ParComp.Pattern as P
-import           ParComp.Pattern (Pattern(..), Cond(..), FunName(..))
-import           ParComp.Parser (chartParse)
+import qualified ParComp.ItemDev.Untyped as U
+import           ParComp.ItemDev.Untyped (Fun(..), Pred(..), IsPatt(..))
+import qualified ParComp.ItemDev.Typed as Ty
+import           ParComp.ItemDev.Typed
+  ( Pattern(..), Op(..), pair, nothing, just, nil, cons
+  , left, right, bimap, guard
+  )
+import           ParComp.ParserDev (chartParse)
 
 import           Debug.Trace (trace)
 
 
--- -- | CFG complete rule
--- complete :: P.Rule T.Text T.Text
--- complete =
---   P.Rule [leftP, rightP] consP P.TrueC
---   where
---     leftP = Pair
---       (Pair (Var "A") (Pair (Var "B") (Var "beta")))
---       (Pair (Var "i") (Var "j"))
---     rightP = Pair
---       (Pair (Var "B") (Const I.Unit))
---       (Pair (Var "j") (Var "k"))
---     consP = Pair
---       (Pair (Var "A") (Var "beta"))
---       (Pair (Var "i") (Var "k"))
+--------------------------------------------------
+-- CFG item
+--------------------------------------------------
 
 
--- | Append two (item) lists.
-append :: I.Item sym -> [I.Item sym]
-append (I.Pair xs ys) =
-  [go xs]
+-- | Item's span
+type Span = (Int, Int)
+
+-- | Grammar node
+type Node = T.Text
+
+-- | Non-terminal/terminal symbol
+type Sym = T.Text
+
+-- | Rule's head
+type Head = Node
+
+-- | Rule's body
+type Body = [Maybe Node]
+
+-- | Dotted rule
+type DotRule = (Head, Body)
+
+-- | Active item
+type Active = (DotRule, Span)
+
+-- | Top-level item
+type Top = Either Active DotRule
+
+
+topItem :: Op repr => repr Active -> repr Top
+topItem = left
+
+topRule :: Op repr => repr DotRule -> repr Top
+topRule = right
+
+active :: Op repr => repr DotRule -> repr Span -> repr Active
+active = pair
+
+rule :: Op repr => repr Head -> repr Body -> repr DotRule
+rule = pair
+
+span :: Op repr => repr Int -> repr Int -> repr Span
+span = pair
+
+pos :: Op repr => Int -> repr Int
+pos = const
+
+head :: Op repr => Node -> repr Head
+head = const
+
+-- | Dot in a dotted rule
+dot :: Op repr => repr (Maybe Node)
+dot = nothing
+
+
+--------------------------------------------------
+-- Utility functions
+--------------------------------------------------
+
+
+-- | Replace any value by unit.
+constF :: b -> Fun a b
+constF x = Fun "constF" $ P.const [x]
+
+
+-- | Append two lists.
+append :: Fun ([a], [a]) [a]
+append = Fun "append" $ \(xs, ys) -> do
+  return (xs ++ ys)
+
+
+-- | Construct a list from a head and a tail.
+consList :: Fun (a, [a]) [a]
+consList = Fun "consList" $ \(x, xs) -> do
+  return (x : xs)
+
+
+-- | Split at dot.
+splitAtDot :: Fun Body (Body, Body)
+splitAtDot = _splitAt "splitAtDot" Nothing
+
+
+-- | Split a list at a given value.
+_splitAt :: (Eq a) => T.Text -> a -> Fun [a] ([a], [a])
+_splitAt txt y =
+  Fun txt ((:[]) . go)
   where
-    go xs =
-      case xs of
-        I.Unit -> ys
-        I.Pair x xs' -> I.Pair x (go xs')
-        _ -> error "append: argument xs not a list"
-append _ = error "append: argument not a pair of lists"
-
-
--- | Split the list at the given item.
-splitAt :: (Eq sym) => I.Item sym -> I.Item sym -> [I.Item sym]
-splitAt item =
-  (:[]) . uncurry I.Pair . go
-  where
-    go list@(I.Pair x xs)
-      | x == item = (I.Unit, list)
+    go list@(x:xs)
+      | x == y = ([], list)
       | otherwise =
-         let (pref, suff) = go xs
-          in (I.Pair x pref, suff)
-    go I.Unit = (I.Unit, I.Unit)
-    go _ = error "splitAt: argument not a list"
+          let (pref, suff) = go xs
+           in (x:pref, suff)
+    go [] = ([], [])
 
 
--- | Local pattern and rule type synonyms
-type Patt = Pattern T.Text T.Text T.Text
-type Rule = P.Rule T.Text T.Text T.Text
+-- | Check if the list ends with dot.  If so, return it as is.
+endsWith :: (Eq a) => T.Text -> a -> Fun [a] [a]
+endsWith txt y = Fun txt $ \xs -> do
+  P.guard $ lastMaybe xs == Just y
+  return xs
 
 
--- | Match suffix which satisfies the given predicate.
-suffix' :: (I.Item sym -> Bool) -> I.Item sym -> [I.Item sym]
-suffix' p xs
-  | p xs = xs : rest
-  | otherwise = rest
-  where
-    rest =
-      case xs of
-        I.Pair x xs' -> suffix' p xs'
-        I.Unit -> []
-        _ -> error "suffix: argument not a list"
+-- | Make sure that the body of the dotted rule ends with the dot.
+endsWithDot :: Fun Body Body
+endsWithDot = endsWith "endsWithDot" Nothing
 
 
--- | Does the list end with dot?
-endsWithDot :: (Eq sym) => I.Item sym -> [I.Item sym]
-endsWithDot = suffix' $ \case
-  I.Pair I.Unit I.Unit -> True
-  _ -> False
+-- | Check if the list ends with dot.  If so, return it as is.
+endsWithP :: (Eq a) => T.Text -> a -> Pred [a]
+endsWithP txt y = Pred txt $ \xs ->
+  lastMaybe xs == Just y
 
 
--- | Match suffix.
-suffix :: Patt -> Patt
--- suffix p = Or p (Pair Any $ suffix p)
-suffix p = Fix $ Or p (Pair Any Rec)
+-- | Does the body of the dotted rule ends with the dot?
+endsWithDotP :: Pred Body
+endsWithDotP = endsWithP "endsWithDotP" Nothing
 
 
--- | Remove suffix starting from the first element which satisfies the given
--- pattern.
-removeSuffix :: Patt -> Patt
-removeSuffix p =
-  Fix $ Or p1 (Or p2 p3)
-  where
-    p1 = Let Any (Pair p Any) nil
-    -- p2 = Pair Any (removeSuffix p)
-    p2 = Pair Any Rec
-    p3 = nil
-    nil = Const I.Unit
+-- | Safe version of `last`
+lastMaybe :: [a] -> Maybe a
+lastMaybe = \case
+  [] -> Nothing
+  [x] -> Just x
+  _ : xs -> lastMaybe xs
 
 
--- -- | Split a list xs into two parts (ys, zs) w.r.t pattern p so that:
--- --
--- -- * ys = removeSuffix p xs
--- -- * zs = suffix p xs
--- --
--- splitAt :: Patt -> Patt
--- splitAt p =
---   Fix $ Or p1 (Or p2 p3)
+-- | Node label
+label :: Fun Node Sym
+label = Fun "label" nodeLabel
+
+
+-- -- | Body element label
+-- labelB :: Fun (Maybe Node) Sym
+-- labelB = Fun "labelB" $ \case
+--   Nothing -> error "labelB: encountered Nothing"
+--   Just x -> nodeLabel x
+
+
+-- | Determine the label of the node.
+nodeLabel :: Node -> [Sym]
+nodeLabel x = case T.splitOn "_" x of
+  [term] -> [term]
+  [nonTerm, _nodeId] -> [nonTerm]
+  _ -> error $ "nodeLabel: unhandled symbol (" ++ T.unpack x ++ ")"
+
+
+--------------------------------------------------
+-- Utility patterns
+--------------------------------------------------
+
+
+-- | Operator synonym to `cons`
+(.:) :: (Op repr) => repr a -> repr [a] -> repr [a]
+(.:) = cons
+infixr 5 .:
+
+
+-- -- | Operator synonym to `pair`
+-- (.+) :: (Op repr) => repr a -> repr b -> repr (a, b)
+-- (.+) = pair
+-- infixr 5 .+
+
+
+-- | Match any suffix that satisfies the given suffix pattern.
+suffix :: (Op repr) => repr [a] -> repr [a]
+suffix p = fix $ or p (any .: rec)
+
+
+-- -- | Remove suffix starting with the given element.
+-- removeSuffixCont
+--   :: forall repr a. (Op repr)
+--   => repr a       -- ^ First element of the suffix
+--   -> repr [a]     -- ^ Suffix matching continuation
+--   -> repr [a]     -- ^ The entire list
+-- removeSuffixCont p cont =
+--   fix $ p1 `or` (p2 `or` p3)
 --   where
---     p1 = Let
---       (LVar "suff")
---       (cons p Any)
---       (pair nil (LVar "suff"))
---     p2 = Let
---       (pair (LVar "x") (pair (LVar "pref") (LVar "suff")))
---       -- NOTE: we could simply write:
---       --   `(cons Any (splitAt p))`
---       -- However, we don't want recursion in our patterns, since this would
---       -- prevent us from comparing them and storing them in dictionaries.
---       -- Explicit recursion with `Fix` and `Rec` solves this problem.
---       (cons Any Rec)
---       (pair (cons (LVar "x") (LVar "pref")) (LVar "suff"))
---     p3 = pair nil nil
---     -- Helper functions
---     nil = Const I.Unit
---     cons = Pair
---     pair = Pair
+--     p1 = letIn any (p .: any) (nil `and` cont)
+--     p2 = any .: rec
+--     p3 = nil `and` cont
 
 
--- -- | A version of `splitAt` which works by manually renaming variables.
--- -- This should not be necessar, though!
--- splitAt' :: Patt -> Patt
--- splitAt' p =
---   go 0
+-- -- | Remove suffix starting with the given element.
+-- removeSuffix :: forall repr a. (Op repr) => repr a -> repr ([a] -> [a])
+-- removeSuffix p =
+--   fix $ p1 `or` (p2 `or` p3)
 --   where
---     go k = 
---       Or p1 (Or p2 p3)
---       where
---         p1 = Let
---           (var "suffix")
---           (cons p Any)
---           (pair nil (var "suffix"))
---         p2 = Let
---           (pair (var "x") (pair (var "pref") (var "suff")))
---           (cons Any (go (k + 1)))
---           (pair (cons (var "x") (var "pref")) (var "suff"))
---         p3 = pair nil nil
---         -- Helper functions
---         nil = Const I.Unit
---         cons = Pair
---         pair = Pair
---         var str = Var $ str `T.append` T.pack (show k)
+--     p1 = letIn (p .: any) nil
+--     p2 = expand (any .: rec)
+--     p3 = expand nil
 
 
--- | CFG complete rule with dots
-complete :: Rule
+-- | Split a list @xs@ into two parts @(ys, zs)@ w.r.t pattern @p@ so that:
+--
+-- * @ys = removeSuffix p xs@
+-- * @zs = suffix p xs@
+--
+splitAt :: forall repr a. (Op repr) => repr a -> repr ([a] -> ([a], [a]))
+splitAt p =
+  fix $ p1 `or` p2
+  where
+    p1 = letIn
+      ((p .: any) `and` local "suff")
+      (pair nil (local "suff"))
+    p2 = letIn
+      (local "x" .: via
+        splitRec
+        (pair (local "xs") (local "ys"))
+      )
+      (pair (local "x" .: local "xs") (local "ys"))
+
+    -- NB: defining and annotating `splitRec` is optional, but it allows to
+    -- verify that the types (of `fix` and `rec`) match.
+    splitRec :: repr ([a] -> ([a], [a]))
+    splitRec = rec
+
+
+-- | Append the first list at the end of the second list.
+appendEnd :: forall repr a. (Op repr) => repr [a] -> repr ([a] -> [a])
+appendEnd ys =
+  fix $ p1 `or` p2
+  where
+    p1 = letIn nil ys
+    p2 = letIn
+      (local "x" .: via rec (local "xs"))
+      (local "x" .: local "xs")
+
+
+-- | Append two lists.
+append' :: forall repr a. (Op repr) => repr [a] -> repr [a] -> repr [a]
+append' xs ys = map (appendEnd ys) xs
+
+
+--------------------------------------------------
+-- Tests
+--------------------------------------------------
+
+
+-- testAppEnd :: Pattern (Body -> Body)
+-- testAppEnd = appendEnd nil
+
+
+-- testX :: Body
+-- testX = [Nothing, Just "a"]
+
+
+-- testMatch :: IO ()
+-- testMatch = U.runMatchT $ do
+--   let f = unPatt testAppEnd
+--       x = U.encodeI testX
+--   it' <- U.match U.Strict f x
+--   U.lift $ do
+--     putStr "f   : " >> print f
+--     putStr "x   : " >> print testX
+--     putStr "it' : " >> print (U.decodeI it' :: Body)
+
+
+-- testApp :: Pattern [Int]
+-- testApp = append'
+--   (const 1 .: const 2 .: nil)
+--   (const 3 .: const 4 .: nil)
+
+
+-- testSplitAt :: Pattern ([Int] -> ([Int], [Int]))
+testSplitAt1 :: Pattern ([Int] -> ([Int], [Int]))
+testSplitAt1 = splitAt (const 1)
+
+
+-- testSplitAtAny :: Pattern ([a] -> ([a], [a]))
+-- testSplitAtAny = splitAt any
+
+
+testX :: [Int]
+testX = [0, 1, 2]
+
+
+testMatch :: IO ()
+testMatch = U.runMatchT $ do
+  let f  = unPatt testSplitAt1
+  -- let f  = unPatt (app . fun $ _splitAt "splitAt 1" (1 :: Int))
+      xs = U.encodeI testX
+  it <- U.match U.Strict f xs
+  U.lift $ do
+    putStr "f : " >> print f
+    putStr "x : " >> print testX
+    putStr "it: " >> print (U.decodeI it :: ([Int], [Int]))
+
+
+--------------------------------------------------
+-- Rules
+--------------------------------------------------
+
+
+-- | CFG complete rule
+complete :: Ty.Rule Top
 complete =
-  P.Rule [leftP, rightP] downP condP
+
+  Ty.Rule [leftP, rightP] downP condP
+
   where
-    leftP = item
-      (rule (Var "A")
-        -- ( Via (splitAt dot)
-        ( Via (App "splitAtDot")
-            (Pair (Var "alpha") (Pair dot (Pair (Var "B") (Var "beta"))))
+
+    leftP = topItem $ active
+      (rule v_A
+        (via (fun splitAtDot)
+        -- (via (splitAt dot)
+          (pair v_alpha (dot .: just v_B .: v_beta))
         )
       )
-      (span "i" "j")
-    rightP = item
-      (rule (Var "C")
-        -- (suffix $ Pair dot nil)
-        (App "endsWithDot")
+      (span v_i v_j)
+
+    rightP = topItem $ active
+      (rule v_C
+        (guard endsWithDotP)
+        -- (suffix $ const dot .: nil)
       )
-      (span "j" "k")
-    condP = Eq
-      (Map "label" (Var "B"))
-      (Map "label" (Var "C"))
-    downP = item
-      (rule (Var "A")
-        ( Map "append" $ Pair
-            (Var "alpha")
-            (Pair (Var "B") (Pair dot (Var "beta")))
+      (span v_j v_k)
+
+    condP = eq
+      (map (fun label) v_B)
+      (map (fun label) v_C)
+
+    downP = topItem $ active
+      (rule v_A
+        -- (append'
+        (bimap append
+          v_alpha
+          (just v_B .: dot .: v_beta)
         )
       )
-      (span "i" "k")
-    -- Some helper functions, to make the code more readable
-    item r s = Union . Left $ Pair r s
-    rule x y = Pair x y
-    cons x y = Pair x y
-    span i j = (Pair (Var i) (Var j))
-    -- The dot is represented just as nil (empty list)
-    dot = unit
-    nil = unit
-    unit = Const I.Unit
+      (span v_i v_k)
+
+    -- Variables and their types
+    v_A = var "A"         :: Pattern Node
+    v_B = var "B"         :: Pattern Node
+    v_C = var "C"         :: Pattern Node
+    v_alpha = var "alpha" :: Pattern Body
+    v_beta = var "beta"   :: Pattern Body
+    v_i = var "i"         :: Pattern Int
+    v_j = var "j"         :: Pattern Int
+    v_k = var "k"         :: Pattern Int
 
 
 -- | CFG predict rule
-predict :: Rule
+predict :: Ty.Rule Top
 predict =
-  P.Rule [leftP, rightP] downP condP
+  Ty.Rule [leftP, rightP] downP condP
   where
-    leftP = item
-      (rule Any
-        -- ( Via (splitAt dot)
-        ( Via (App "splitAtDot")
-            (Pair Any (Pair dot (Pair (Var "B") Any)))
-        )
+    leftP = topItem $ active
+      (rule any
+        (suffix $ dot .: just (var "B") .: any)
       )
-      (span "i" "j")
-    rightP = Union . Right $ Via (rule (Var "C") Any) (Var "rule")
-    condP = Eq
-      (Map "label" (Var "B"))
-      (Map "label" (Var "C"))
-    downP = item
-      (Var "rule")
-      (span "j" "j")
-    -- Some helper functions, to make the code more readable
-    item r s = Union . Left $ Pair r s
-    rule x y = Pair x y
-    cons x y = Pair x y
-    span i j = (Pair (Var i) (Var j))
-    -- The dot is represented just as nil (empty list)
-    dot = unit
-    unit = Const I.Unit
+      (span (var "i") (var "j"))
+    rightP = topRule $
+      rule (var "C") (var "alpha")
+    condP = eq
+      (map (fun label) (var "B"))
+      (map (fun label) (var "C"))
+    downP = topItem $ active
+      (rule (var "C") (var "alpha"))
+      (span (var "j") (var "j"))
 
 
--- | Compute the base items for the given sentence and grammar
-cfgBaseItems 
+--------------------------------------------------
+-- Axioms 
+--------------------------------------------------
+
+
+-- | Enumerate base items for the given sentence and grammar.
+cfgBaseItems
   :: [T.Text]
     -- ^ Input sentence
   -> S.Set (T.Text, [T.Text])
     -- ^ CFG rules
-  -> S.Set (I.Item T.Text)
+  -> [Top]
 cfgBaseItems inp cfgRules =
-  S.fromList $ base1 ++ base2 ++ baseRules
+  base1 ++ base2 ++ baseRules
   where
     n = length inp
     base1 = do
-      -- Note that we use prediction
-      -- i <- [0..n-1]
-      i <- [0]
-      (ruleHead, ruleBody) <- S.toList cfgRules
-      let rule = mkRule ruleHead ruleBody
-          span = mkSpan i i
-      return $ mkItem rule span
+      (hd, bd) <- S.toList cfgRules
+      return $ Left (mkRule hd bd, (0, 0))
     base2 = do
       (i, term) <- zip [0..n-1] inp
-      let rule = mkRule term []
-          span = mkSpan i (i + 1)
-      return $ mkItem rule span
+      return $ Left (mkRule term [], (i, i + 1))
     baseRules = do
-      (ruleHead, ruleBody) <- S.toList cfgRules
-      let rule = mkRule ruleHead ruleBody
-      return . I.Union $ Right rule
-    mkItem rl sp = I.Union . Left $ I.Pair rl sp
-    mkRule hd bd = I.Pair (I.Sym hd) (I.list $ dot : map I.Sym bd)
-    mkSpan i j = I.Pair (pos i) (pos j)
-    pos = I.Sym . T.pack . show
-    -- The dot is represented just as nil (empty list)
-    dot = I.Unit
+      (hd, bd) <- S.toList cfgRules
+      return $ Right (mkRule hd bd)
+    mkRule hd bd = (hd, Nothing : fmap Just bd)
+
+
+--------------------------------------------------
+-- Main 
+--------------------------------------------------
 
 
 testCFG :: IO ()
@@ -294,35 +454,21 @@ testCFG = do
         , ("V_26", ["runs"])
         , ("Adv_27", ["quickly"])
         ]
-      label = \case
-        I.Sym x ->
-          case T.splitOn "_" x of
-            [term] -> [I.Sym term]
-            [nonTerm, _nodeId] -> [I.Sym nonTerm]
-            _ -> error $ "label: unhandled symbol (" ++ T.unpack x ++ ")"
-        x -> error $ "label: unhandled item (" ++ show x ++ ")"
-      cfgFunSet = P.emptyFunSet
-        { P.funMap = M.fromList
-            [ ("append", append)
-            , ("label", label)
-            , ("splitAtDot", splitAt I.Unit)  -- I.Unit represents dot
-            , ("endsWithDot", endsWithDot)
-            ]
-        }
+--         [ ("NP_3", ["DET_4", "N_5"])
+--         , ("DET_21", ["a"])
+--         , ("N_23", ["dog"])
+--         ]
       sent = ["a", "dog", "quickly", "eats", "some", "pizza"]
+--       sent = ["a", "dog"]
       baseItems = cfgBaseItems sent cfgRules
       ruleMap = M.fromList
         [ ("CO", complete)
         , ("PR", predict)
         ]
-      pos = I.Sym . T.pack . show
       zero = pos 0
       slen = pos (length sent)
-      isFinal = \case
-        I.Union (Left (I.Pair _ (I.Pair i j)))
-          | i == zero && j == slen -> True
-        _ -> False
-  -- forM_ (S.toList baseItems) print
-  chartParse cfgFunSet baseItems ruleMap isFinal >>= \case
+      finalPatt = topItem $ active any (span zero slen)
+  -- forM_ baseItems print
+  chartParse baseItems ruleMap finalPatt >>= \case
     Nothing -> putStrLn "# No parse found"
-    Just it -> print it 
+    Just it -> print it
